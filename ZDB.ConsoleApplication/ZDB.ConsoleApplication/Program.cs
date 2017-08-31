f882ee54a10a45de717cdce7a97388befd8ec316
zhangdb <466598043@qq.com>
2017-08-31 14:21:01 +0800

    生成唯一ID
    生产唯一ID方法

---
+++ ZDB.ConsoleApplication/ZDB.ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ZDB.GenerateUniqueID;
+
+namespace ZDB.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uniqueID = MadeUniqueID.GenerateUniqueID();
+            Console.WriteLine(uniqueID);
+            Console.Read();
+        }
+    }
+}
---
+++ ZDB.ConsoleApplication/ZDB.GenerateUniqueID/MadeUniqueID.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ZDB.GenerateUniqueID
+{
+    public class MadeUniqueID
+    {
+        /// <summary>
+        /// 根据GUID获取16位的唯一字符串
+        /// Author  : 付义方  
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidTo16String()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+                i *= ((int) b + 1);
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+
+        /// <summary>
+        /// 根据GUID获取19位的唯一数字序列
+        /// Author  : 付义方  
+        /// </summary>
+        /// <returns></returns>
+        public static long GuidToLongID()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        /// <summary>
+        /// 生成22位唯一的数字 并发可用
+        /// Author  : 付义方  
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateUniqueID()
+        {
+            System.Threading.Thread.Sleep(1); //保证yyyyMMddHHmmssffff唯一
+            Random d = new Random(BitConverter.ToInt32(Guid.NewGuid().ToByteArray(), 0));
+            string strUnique = DateTime.Now.ToString("yyyyMMddHHmmssffff") + d.Next(1000, 9999);
+            return strUnique;
+        }
+    }
+}
