0c58d07c1ac183234dbcc11845528069e633e9f2
zhangdb <466598043@qq.com>
2017-09-14 14:00:07 +0800

    pdf转image
    pdf转图片

--- ZDB.ConsoleApplication/ZDB.ConsoleApplication/Program.cs
+++ ZDB.ConsoleApplication/ZDB.ConsoleApplication/Program.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using O2S.Components.PDFRender4NET;
 using ZDB.DBRepository.DbFactory;
 using ZDB.DBRepository.Entity;
 using ZDB.DBRepository.SQLite;
 using ZDB.GenerateUniqueID;
+using ZDB.Images.QRCode;
 
 namespace ZDB.ConsoleApplication
 {
@@ -18,6 +23,7 @@ namespace ZDB.ConsoleApplication
             //Console.WriteLine(uniqueID);
 
             #region MyRegion
+
             //try
             //{
             //    var watch = new Stopwatch();
@@ -49,12 +55,123 @@ namespace ZDB.ConsoleApplication
 
             #endregion
 
-            using (var helper=new SQLiteHelper("170826000001"))
-            {
-                var dd = helper.Query("select *from PKG_SKU_LIST",new SQLiteParameter {});
-            }
+            //using (var helper=new SQLiteHelper("170826000001"))
+            //{
+            //    var dd = helper.Query("select *from PKG_SKU_LIST",new SQLiteParameter {});
+            //}
+            //try
+            //{
+            //    Bitmap image = QRCode.GeneratorQrImage("zhang", 300, 300);
+            //    image.Save(@"C:\Users\admin\Desktop\新建文件夹\ddd.png");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //    throw;
+            //}
+            //var source=new List<int>
+            //{
+            //    1,2,3,4,5,6,7,8,9
+            //};
 
+            //var dd = source.Where(x =>
+            //{
+            //    var idd = false;
+            //    if (x == 1||x==7)
+            //    {
+            //        idd = true;
+            //    }
+            //    return idd;
+            //}).FirstOrDefault();
+
+            //var dd = 11 / 12;
+
+            //var ddd = new List<List<string>>
+            //{
+            //    new List<string>
+            //    {
+            //        "a1",
+            //        "a2"
+            //    },
+            //    new List<string>
+            //    {
+            //        "b1",
+            //        "b2",
+            //        "b3"
+            //    },
+            //    new List<string>
+            //    {
+            //        "c1",
+            //        "c2",
+            //        "c3",
+            //        "c4"
+            //    }
+            //};
+
+            //var dd = new List<List<List<string>>>
+            //{
+            //    ddd,
+            //    ddd
+            //};
+
+            //var c1 = new string[] { "帽子1", "帽子2", "帽子3" };
+            //var c2 = new string[] { "上衣1", "上衣2", "上衣3" };
+            //var c3 = new string[] { "裤子a", "裤子b" };
+            //var c4 = new string[] { "房子1", "房子2", "房子3", "房子4" };
+            //CartesianProduct(new int[] { c1.Length, c2.Length, c3.Length,c4.Length }, (result, len) =>
+            //{
+            //    Console.WriteLine("{0},{1},{2},{3}", c1[result[0]], c2[result[1]], c3[result[2]], c4[result[3]]);
+            //    return true;
+            //});
+            //dd[0][0].Add("bb");
+
+            
+            using (var pdf = PDFFile.Open(@"C:\Users\admin\Desktop\1\11.pdf"))
+            {
+                for (var i = 1; i <= pdf.PageCount; i++)
+                {
+                    
+                    using (var pageImage = pdf.GetPageImage(i - 1, 56 * 3))
+                    {
+                        //图片的路径及名称
+                        var ImgPath = @"C:\Users\admin\Desktop\1\" + DateTime.Now.ToString("yyMMddHHmmss") + ".jpg";
+
+                        //覆盖保存图片
+                        pageImage.Save(ImgPath, ImageFormat.Jpeg);
+                    }
+                }
+            }
             Console.Read();
+        }
+
+        public delegate bool SetAlgorithmCallback(int[] result, int length);
+        static bool CartesianProduct(int[] sets, int i, int[] result, SetAlgorithmCallback callback)
+        {
+            for (var j = 0; j < sets[i]; ++j)
+            {
+                result[i] = j;
+                if (i == sets.Length - 1)
+                {
+                    if (!callback(result, result.Length))
+                        return false;
+                }
+                else
+                {
+                    if (!CartesianProduct(sets, i + 1, result, callback))
+                        return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// 求集合笛卡尔积
+        /// </summary>
+        /// <param name="sets">包含集合元素个数的数组</param>
+        /// <param name="callback">回调函数</param>
+        public static void CartesianProduct(int[] sets, SetAlgorithmCallback callback)
+        {
+            int[] result = new int[sets.Length];
+            CartesianProduct(sets, 0, result, callback);
         }
     }
 }
---
+++ ZDB.ConsoleApplication/ZDB.Images/QRCode/QRCode.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using BarcodeWriter = ZXing.Presentation.BarcodeWriter;
+
+namespace ZDB.Images.QRCode
+{
+    public class QRCode
+    {
+        /// <summary>
+        /// 生成二维码图片
+        /// </summary>
+        /// <param name="contents">要生成二维码包含的信息</param>
+        /// <param name="width">生成的二维码宽度（默认300像素）</param>
+        /// <param name="height">生成的二维码高度（默认300像素）</param>
+        /// <returns>二维码图片</returns>
+        public static Bitmap GeneratorQrImage(string contents, int width = 300, int height = 300)
+        {
+            if (string.IsNullOrEmpty(contents))
+            {
+                return null;
+            }
+            //本文地址：http://www.cnblogs.com/Interkey/p/qrcode.html
+            EncodingOptions options = null;
+            BarcodeWriter writer = null;
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = width,
+                Height = height,
+                ErrorCorrection = ErrorCorrectionLevel.H,
+            };
+            writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+
+            //WriteableBitmap dd = writer.Write(contents);
+            Bitmap bitmap = BitmapFromWriteableBitmap(writer.Write(contents)) ;
+
+            return bitmap;
+        }
+
+        /// <summary>
+        /// 生成中间带有图片的二维码图片
+        /// </summary>
+        /// <param name="contents">要生成二维码包含的信息</param>
+        /// <param name="middleImg">要生成到二维码中间的图片</param>
+        /// <param name="width">生成的二维码宽度（默认300像素）</param>
+        /// <param name="height">生成的二维码高度（默认300像素）</param>
+        /// <returns>中间带有图片的二维码</returns>
+        public static Bitmap GeneratorQrImage(string contents, Image middleImg, int width = 300, int height = 300)
+        {
+            if (string.IsNullOrEmpty(contents))
+            {
+                return null;
+            }
+            if (middleImg == null)
+            {
+                //return null;
+                return GeneratorQrImage(contents);
+            }
+            ////本文地址：http://www.cnblogs.com/Interkey/p/qrcode.html
+            //构造二维码写码器
+            MultiFormatWriter mutiWriter = new MultiFormatWriter();
+            Dictionary<EncodeHintType, object> hint = new Dictionary<EncodeHintType, object>();
+            hint.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hint.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+            //生成二维码
+            BitMatrix bm = mutiWriter.encode(contents, BarcodeFormat.QR_CODE, width, height, hint);
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            Bitmap bitmap = BitmapFromWriteableBitmap(barcodeWriter.Write(bm));
+            //获取二维码实际尺寸（去掉二维码两边空白后的实际尺寸）
+            int[] rectangle = bm.getEnclosingRectangle();
+
+            //计算插入图片的大小和位置
+            int middleImgW = Math.Min((int)(rectangle[2] / 3.5), middleImg.Width);
+            int middleImgH = Math.Min((int)(rectangle[3] / 3.5), middleImg.Height);
+            int middleImgL = (bitmap.Width - middleImgW) / 2;
+            int middleImgT = (bitmap.Height - middleImgH) / 2;
+
+            //将img转换成bmp格式，否则后面无法创建 Graphics对象
+            Bitmap bmpimg = new Bitmap(bitmap.Width, bitmap.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmpimg))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.DrawImage(bitmap, 0, 0);
+            }
+
+            //在二维码中插入图片
+            Graphics myGraphic = Graphics.FromImage(bmpimg);
+            //白底
+            myGraphic.FillRectangle(Brushes.White, middleImgL, middleImgT, middleImgW, middleImgH);
+            myGraphic.DrawImage(middleImg, middleImgL, middleImgT, middleImgW, middleImgH);
+            return bmpimg;
+        }
+
+        private static System.Drawing.Bitmap BitmapFromWriteableBitmap(WriteableBitmap writeBmp)
+        {
+            System.Drawing.Bitmap bmp;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create((BitmapSource)writeBmp));
+                enc.Save(outStream);
+                bmp = new System.Drawing.Bitmap(outStream);
+            }
+            return bmp;
+        }
+
+    }
+}
